027756bb99fc731fbeb65afac1808bb1cc36242d
rajesh6334 <118992210+rajesh6334@users.noreply.github.com>
2022-11-26 21:30:30 +0530

    Add files via upload

---
+++ right shift.py
@@ -0,0 +1,12 @@
+#right shift
+a=int(input())
+print('right shift values')
+print('right shift 1',a>>1)
+print('right shift 2',a>>2)
+print('right shift 3',a>>3)
+print('right shift 4',a>>4)
+print('right shift 5',a>>5)
+print('right shift 6',a>>6)
+print('right shift 7',a>>7)
+print('right shift 8',a>>8)
+print('right shift 9',a>>9)
